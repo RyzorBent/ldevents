e513c4741b641be3c160acd4187f8f2872c526a2
RyzorBent <zestadaryzor@gmail.com>
2024-01-14 13:44:59 +0200

    touch ups

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,7 +3,7 @@ import { sections } from '../styles/sections.module.css';
 
 export default function ContactForm() {
   return (
-    <div className={`${sections} bg-gray-700 mt-6 mb-16 p-6 text-white rounded-lg`}>
+    <div className={`${sections} bg-gray-700 mt-6 mb-16 p-6 text-white `}>
       <h2 className='text-center text-5xl font-bold pt-6'>Contact Idevents for Event Planning</h2>
       <p className='text-center mt-4 mb-8 text-white'>
         Contact Idevents for all your event planning needs. Get in touch with us today to find out
@@ -22,14 +22,14 @@ export default function ContactForm() {
             type='text'
             name='first_name'
             placeholder='First Name'
-            className='bg-gray-800 border border-gray-600 text-white placeholder-gray-400 p-3 rounded-md'
+            className='border border-gray-100 text-white placeholder-gray-400 p-3 rounded-md'
             required
           />
           <input
             type='text'
             name='last_name'
             placeholder='Last Name'
-            className='bg-gray-800 border border-gray-600 text-white placeholder-gray-400 p-3 rounded-md'
+            className='border border-gray-600 text-white placeholder-gray-400 p-3 rounded-md'
             required
           />
         </div>
@@ -37,20 +37,20 @@ export default function ContactForm() {
           type='email'
           name='email'
           placeholder='Email'
-          className='bg-gray-800 border border-gray-600 text-white placeholder-gray-400 p-3 rounded-md'
+          className='border border-gray-600 text-white placeholder-gray-400 p-3 rounded-md'
           required
         />
         <textarea
           name='message'
           rows='3'
           placeholder='Type your message here.'
-          className='bg-gray-800 border border-gray-600 text-white placeholder-gray-400 p-3 rounded-md'
+          className='border border-gray-600 text-white placeholder-gray-400 p-3 rounded-md'
           required
         ></textarea>
         <div className='flex justify-between items-center mt-6'>
           <button
             type='submit'
-            className='bg-gray-800 text-white py-2 px-4 rounded-md hover:bg-gray-600 transition duration-150 ease-in-out'
+            className='text-white py-2 px-4 rounded-md hover:bg-gray-600 transition duration-150 ease-in-out'
           >
             Send
           </button>
